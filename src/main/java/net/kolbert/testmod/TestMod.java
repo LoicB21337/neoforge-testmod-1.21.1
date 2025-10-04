76196c6a7ac9cbdcee7b0eef0f3c5323f4957464
LoicB21337 <loic.brunet80@gmail.com>
2025-10-04 20:14:57 +0200

    Added bismuth Blocks & ores Added Chisel Added Magic Block

--- src/main/java/net/kolbert/testmod/TestMod.java
+++ src/main/java/net/kolbert/testmod/TestMod.java
@@ -1,5 +1,9 @@
 package net.kolbert.testmod;
 
+import net.kolbert.testmod.block.ModBlocks;
+import net.kolbert.testmod.item.ModCreativeModeTabs;
+import net.kolbert.testmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -33,6 +37,11 @@ public class TestMod {
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
+        ModCreativeModeTabs.register(modEventBus);
+
+        ModItems.ITEMS.register(modEventBus);
+        ModBlocks.BLOCKS.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -46,7 +55,14 @@ public class TestMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if (event.getTabKey()== CreativeModeTabs.INGREDIENTS){
+            event.accept(ModItems.BISMUTH);
+            event.accept(ModItems.RAW_BISMUTH);
+        }
+        if (event.getTabKey()== CreativeModeTabs.BUILDING_BLOCKS){
+            event.accept(ModBlocks.BISMUTH_BLOCK);
+            event.accept(ModBlocks.BISMUTH_ORE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/net/kolbert/testmod/item/ModItems.java
@@ -0,0 +1,20 @@
+package net.kolbert.testmod.item;
+
+import net.kolbert.testmod.TestMod;
+import net.kolbert.testmod.item.custom.ChiselItem;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(TestMod.MOD_ID);
+
+    public static final DeferredItem<Item> BISMUTH = ITEMS.register("bismuth",() -> new Item(new Item.Properties()));
+    public static final DeferredItem<Item> RAW_BISMUTH = ITEMS.register("raw_bismuth",() -> new Item(new Item.Properties()));
+    public static final DeferredItem<Item> CHISEL = ITEMS.register("chisel",() -> new ChiselItem(new Item.Properties().durability(32)));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
