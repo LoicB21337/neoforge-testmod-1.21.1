2c55d06bb997a2bd0b8ce8b7615e1b5823db2a28
LoicB21337 <loic.brunet80@gmail.com>
2025-10-29 15:20:15 +0100

    Added bismuth Hammer and bismuth Armor

--- src/main/java/net/kolbert/testmod/item/ModCreativeModeTabs.java
+++ src/main/java/net/kolbert/testmod/item/ModCreativeModeTabs.java
@@ -34,6 +34,13 @@ public class ModCreativeModeTabs {
                         output.accept(ModItems.BISMUTH_AXE);
                         output.accept(ModItems.BISMUTH_HOE);
 
+                        output.accept(ModItems.BISMUTH_HAMMER);
+
+                        output.accept(ModItems.BISMUTH_HELMET);
+                        output.accept(ModItems.BISMUTH_CHESTPLATE);
+                        output.accept(ModItems.BISMUTH_LEGGINGS);
+                        output.accept(ModItems.BISMUTH_BOOTS);
+
                     })
                     .build());
 
